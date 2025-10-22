dbee02e66541f5127fa6e92e9aa159919d2646d3
Akash Anchaneyalu <akashanchaneyalu@gmail.com>
2025-10-22 19:47:57 +0530

    Update Terraform module files

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,7 +1,11 @@
 output "instance_id" {
+  description = "The IDs of the EC2 instances being created"
+  value       = aws_instance.web[*].id
   
 }
 
 output "public_ip" {
+  description = "The public IPs of the EC2 instances"
+  value       = aws_instance.web[*].public_ip
   
 }
