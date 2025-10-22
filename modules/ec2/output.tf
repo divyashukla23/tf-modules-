e51e92bdedef9abcd7a7c2c9a84aaad0a18355d3
Divya Shukla <divyashukla20993@gmail.com>
2025-10-22 19:28:12 +0530

    feat: test

---
+++ modules/ec2/output.tf
@@ -0,0 +1,7 @@
+output "instance_id" {
+  
+}
+
+output "public_ip" {
+  
+}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+}
+
+variable "security_groups" {
+  description = "List of Security Group IDs"
+  type        = list(string)
+}
+
+variable "subnet_id" {
+  description = "Subnet ID for EC2 instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Tag name for instance"
+  type        = string
+}
